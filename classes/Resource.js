5b18c7566033fd6a82f326d64d06167059c9c3fe
Ben Timbrell <bmtimbrell@gmail.com>
2024-07-31 08:42:26 +0100

    made resource hitbox bigger

--- classes/Resource.js
+++ classes/Resource.js
@@ -4,8 +4,8 @@ export default class Resource {
     constructor(game) {
         this.x = Math.random() * (game.canvas.width - Cell.cellSize);
         this.y = Math.floor((Math.random() * 5) + 1) * Cell.cellSize + 40;
-        this.width = 10;
-        this.height = 20;
+        this.width = 20;
+        this.height = 30;
         this.amounts = [10, 20, 30, 40, 50, 60];
         this.amountsIndex = Math.floor(Math.random() * this.amounts.length + game.luck);
         this.amount = this.amountsIndex >= this.amounts.length ? this.amounts[this.amounts.length - 1]
@@ -22,12 +22,12 @@ export default class Resource {
             this.image, 
             0, 
             0, 
-            this.width, 
-            this.height, 
-            this.x, 
-            this.y, 
-            this.width, 
-            this.height
+            this.width - 10, 
+            this.height - 10, 
+            this.x + 5, 
+            this.y + 5, 
+            this.width - 10, 
+            this.height - 10
         );
     }
 }
